5cfad9f3f093080cf0f6c1c53ea78b969bcb2998
ZTAS0283 <ankit.sen@zehntech.com>
2025-10-06 18:58:26 +0530

    changed Dp

--- app/layout.tsx
+++ app/layout.tsx
@@ -113,7 +113,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="retro"
           enableSystem
           themes={[
             "light",
